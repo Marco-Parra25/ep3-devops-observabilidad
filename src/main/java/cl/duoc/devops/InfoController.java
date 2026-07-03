c07e932fcedb1c48cde32ffb36a1b3748950d299
Marco Parra <marco.parra250511@gmail.com>
2026-07-02 22:15:38 -0400

    demo: version 2.0.0 (#17)

--- src/main/java/cl/duoc/devops/InfoController.java
+++ src/main/java/cl/duoc/devops/InfoController.java
@@ -16,7 +16,7 @@ import java.util.Map;
 public class InfoController {
 
 
-    private static final String APP_VERSION = "1.0.0";
+    private static final String APP_VERSION = "2.0.0";
 
     @GetMapping("/version")
     public Map<String, String> version() {
